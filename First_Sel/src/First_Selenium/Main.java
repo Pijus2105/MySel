989a8bfa35caae0a43c709d2cc0d1c294e81926a
Elphill <Elphill@DESKTOP-QEBQ8BA>
2022-10-12 11:52:50 +0530

    My 1st commit

---
+++ First_Sel/src/First_Selenium/Main.java
@@ -0,0 +1,37 @@
+package First_Selenium;
+
+public class Main {
+
+	public static void main(String[] args)  {
+
+     String s = "123elphill@#!Tech456"; 
+     int count=0;
+     
+      String NotAlpha="";
+   //  s = s.replaceAll("[^a-zA-Z]", ""); //regex=regular expression
+     for(int i=0 ;i<s.length();i++) 
+     {
+    	 if(!Character.isLetter(s.charAt(i))&&!Character.isWhitespace(s.charAt(i))) 
+    	 {
+    		count++; 
+    		 
+    	 }else 
+    	 {
+    		 NotAlpha=NotAlpha+s.charAt(i);
+    	 } 
+    	  
+     }
+     
+     if(count==0) 
+     {
+    	 System.out.println(" Threre is not Alpha Character");
+     }else 
+     {
+    	 System.out.println(" Threre  Alpha Character " +NotAlpha );
+     }
+     
+  //  System.out.println(NotAlpha);
+	        
+	}
+
+}
